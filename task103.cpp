e7d988604ece7fad5c596579dae1457ab178ed1a
addin ahmed diganta <Shafia.nodi1997@gmail.com>
2026-04-05 23:32:55 +0600

    task 103 updated

--- task103.cpp
+++ task103.cpp
@@ -2,24 +2,28 @@
 #include <iostream>
 #include <cmath>
 
+
 const int SCREEN_WIDTH = 800;
 const int SCREEN_HEIGHT = 600;
 
 struct Circle {
-    float x, y, r;
-    SDL_Color color;
+    float x, y;     
+    float r;  //radus of circle
+    SDL_Color color; 
 };
 
-// Function to calculate distance and check collision
 bool isColliding(Circle c1, Circle c2) {
+    
     float dx = c1.x - c2.x;
-    float dy = c1.y - c2.y;
-    float distance = sqrt(dx * dx + dy * dy);
+    float dy = c1.y - c2.y;//using pythagoras theorem
+    float distance = sqrt(dx * dx + dy * dy); // If distance is less than both radii combined, they are touching
     return distance < (c1.r + c2.r);
 }
 
 void drawCircle(SDL_Renderer* renderer, Circle c) {
+    
     SDL_SetRenderDrawColor(renderer, c.color.r, c.color.g, c.color.b, c.color.a);
+    
     for (int w = 0; w < c.r * 2; w++) {
         for (int h = 0; h < c.r * 2; h++) {
             int dx = (int)c.r - w;
@@ -36,8 +40,10 @@ int main(int argc, char* argv[]) {
     SDL_Window* window = SDL_CreateWindow("Task 103: Collision", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    Circle autoC = {0, SCREEN_HEIGHT / 2.0f, 30, {255, 255, 255, 255}};
-    Circle userC = {SCREEN_WIDTH / 2.0f, 100, 30, {0, 255, 255, 255}};
+    Circle autoC = {0, SCREEN_HEIGHT / 2.0f, 30, {255, 255, 255, 255}};//circle that would move continously
+    
+    
+    Circle userC = {SCREEN_WIDTH / 2.0f, 100, 30, {0, 255, 255, 255}};//the circle we have to move
 
     bool running = true;
     SDL_Event e;
@@ -47,35 +53,47 @@ int main(int argc, char* argv[]) {
             if (e.type == SDL_QUIT) running = false;
         }
 
-        // 1. Handle User Input
         const Uint8* keystate = SDL_GetKeyboardState(NULL);
         if (keystate[SDL_SCANCODE_UP])    userC.y -= 5;
         if (keystate[SDL_SCANCODE_DOWN])  userC.y += 5;
         if (keystate[SDL_SCANCODE_LEFT])  userC.x -= 5;
         if (keystate[SDL_SCANCODE_RIGHT]) userC.x += 5;
 
-        // 2. Autonomous Motion
-        autoC.x += 3;
-        if (autoC.x - autoC.r > SCREEN_WIDTH) autoC.x = -autoC.r;
+    
+        if (userC.x - userC.r < 0) userC.x = userC.r;//circle that we can move doesnot cross left screen
+        if (userC.x + userC.r > SCREEN_WIDTH) userC.x = SCREEN_WIDTH - userC.r;//circle that we use doesnot cross right screen
+        
+        if (userC.y - userC.r < 0) userC.y = userC.r;//circle that we can move doesnot cross left screen
+        if (userC.y + userC.r > SCREEN_HEIGHT) userC.y = SCREEN_HEIGHT - userC.r;//circle that we use doesnot cross right screen
 
-        // 3. Collision Check & Visual Effect
+
+        autoC.x += 3;
+
+        if (autoC.x - autoC.r > SCREEN_WIDTH)
+        {autoC.x = -autoC.r;//if moving circle crosses the left side
+        }
+  
         if (isColliding(autoC, userC)) {
-            autoC.color = {255, 0, 0, 255}; // Turn Red
+            autoC.color = {255, 0, 0, 255}; //if collided then color changes to red
             userC.color = {255, 0, 0, 255};
         } else {
-            autoC.color = {255, 255, 255, 255}; // Reset colors
-            userC.color = {0, 255, 255, 255};
+           
+            autoC.color = {255, 255, 255, 255}; 
+            userC.color = {0, 255, 255, 255};//no color change
         }
 
-        // 4. Rendering
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
         
+   
         drawCircle(renderer, autoC);
         drawCircle(renderer, userC);
 
-        SDL_RenderPresent(renderer);
-        SDL_Delay(16); // ~60 FPS
+      
+        SDL_RenderPresent(renderer);//show the outcomes
+        
+        
+        SDL_Delay(16);
     }
 
     SDL_DestroyRenderer(renderer);
