21ddb56e4c16f4abd89e1a0082c4338bc6fbde5f
addin ahmed diganta <Shafia.nodi1997@gmail.com>
2026-04-06 10:52:12 +0600

    task101 updated

--- task101.cpp
+++ task101.cpp
@@ -1,18 +1,17 @@
 #include <SDL2/SDL.h>
 #include <math.h>
 
-// Modified function to draw a FILLED circle instead of just an outline
+
 void drawFilledCircle(SDL_Renderer* renderer, int centerX, int centerY, int radius) {
-    // We create a bounding box around the circle
+   
     for (int w = 0; w < radius * 2; w++) {
         for (int h = 0; h < radius * 2; h++) {
-            // Calculate distance from the center of the bounding box
+         
             int dx = radius - w; 
             int dy = radius - h;
             
-            // Pythagorean theorem: If distance squared <= radius squared, it's inside
             if ((dx * dx + dy * dy) <= (radius * radius)) {
-                SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);
+                SDL_RenderDrawPoint(renderer, centerX + dx, centerY + dy);//using pythagoras theorem
             }
         }
     }
@@ -26,7 +25,7 @@ int main(int argc, char* argv[]) {
     
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    int running = 1; // In C, we often use 1 for true
+    int running = 1; 
     SDL_Event event;
 
     while (running) {
@@ -34,17 +33,15 @@ int main(int argc, char* argv[]) {
             if (event.type == SDL_QUIT) running = 0;
         }
 
-        // 1. Background Color: RED (255, 0, 0)
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);//background color set to black
         SDL_RenderClear(renderer);
 
-        // 2. Circle Color: GREEN (0, 255, 0)
-        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
-        
-        // Use the new filled circle function
+        SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);//cicrle color logic
+    
         drawFilledCircle(renderer, 400, 300, 150);
 
-        // 3. Show the result
+        
         SDL_RenderPresent(renderer);
     }
 
